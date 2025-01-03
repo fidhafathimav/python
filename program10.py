4597a9fd0e502a246b7ceec78127f7976984e3c7
fidhafathimav <fidha9296@gmail.com>
2025-01-03 15:44:03 +0530

    Delete program10.py

--- program10.py
+++
@@ -1,7 +0,0 @@
-def exchange_first_last(str):
-    if len(str)<=1:
-        return str
-    return str[-1]+str[1:-1]+str[0]
-input_string=input("enter a string:")
-result=exchange_first_last(input_string)
-print("modified string:",result)
